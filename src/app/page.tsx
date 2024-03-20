80f2787c9136462d8bd2c3f041c665d45c681491
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-03-21 00:04:57 +0200

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div id='stars'></div>
+      <div id='stars2'></div>
+      <div id='stars3'></div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+
+    </main>
+    </>
+  );
+}
