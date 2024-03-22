35d5a68608bf6f96350e44e77b4ebe43197101c0
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-03-22 23:29:58 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,29 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div id='stars'></div>
-      <div id='stars2'></div>
-      <div id='stars3'></div>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    
+    <main className="flex min-h-screen flex-col items-center justify-between">
+    
+    <section className="welcome">
+    <article className="box">
+        <div className="title">
+            <span className="block"></span>
+            <h1>I'm Nikolay Gavazov<span></span></h1>
+        </div>
 
+        <div className="role">
+            <div className="block"></div>
+            <p>Full Stack Web Developer</p>
+        </div>
+    </article>
+    </section>
+    
     </main>
+    <div>
+    <div id='stars'></div>
+    <div id='stars2'></div>
+    <div id='stars3'></div>
+    </div>
     </>
   );
 }
