d8ab80527a725ea4f6e4e8cfdc82c8653eeb4395
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-03-25 21:58:38 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    
     <main className="flex min-h-screen flex-col items-center justify-between">
-    
     <section className="welcome">
     <article className="box">
         <div className="title">
