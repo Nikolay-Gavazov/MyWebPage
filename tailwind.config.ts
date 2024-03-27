fb78654b72312cc778ec95f3b91f8bfc64401d22
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-03-27 22:26:20 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,10 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens:{
+      'sm': {'max': '639px'},
+      'lg': {'min': '640px'},
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
