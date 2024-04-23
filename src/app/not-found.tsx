21f22e5c37c584b03d34bd474912fbdbe32e058b
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-04-24 00:49:19 +0300

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,23 @@
-import {Fira_Mono } from "next/font/google";
+import { Fira_Mono } from "next/font/google";
 import Link from "next/link";
 import "@styles/notFound.css";
 
-const fira_mono = Fira_Mono({weight:"400",subsets:['latin',"cyrillic"]});
+const fira_mono = Fira_Mono({ weight: "400", subsets: ["latin", "cyrillic"] });
 
 export default function NotFound() {
   return (
     <main className="notFoundMain">
       <section className={fira_mono.className}>
-      <article className="notFound" title="404">404</article>
-      <article className="notFoundText" title="Page Not Found">Page Not Found</article>
-      <div className="link">
-      <Link  href="/">
-        Return Home
-      </Link>
-      </div>
-    </section>
+        <article className="notFound" title="404">
+          404
+        </article>
+        <article className="notFoundText" title="Page Not Found">
+          Page Not Found
+        </article>
+        <div className="link">
+          <Link href="/">Return Home</Link>
+        </div>
+      </section>
     </main>
   );
 }
