6b4ed15ba68e91154cd9f051dee3ef38fa004cd8
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-04-24 00:49:21 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,21 @@ export const metadata = {
 export default function Home() {
   return (
     <>
-        <section className="welcome flex h-full flex-col items-center justify-between">
-          <article className="box">
-            <div className="title">
-              <span className="block"></span>
-              <h1>
-                I'm Nikolay Gavazov<span></span>
-              </h1>
-            </div>
+      <section className="welcome flex h-full flex-col items-center justify-between">
+        <article className="box">
+          <div className="title">
+            <span className="block"></span>
+            <h1>
+              I'm Nikolay Gavazov<span></span>
+            </h1>
+          </div>
 
-            <div className="role">
-              <div className="block"></div>
-              <p>Full Stack Web Developer</p>
-            </div>
-          </article>
-        </section>
+          <div className="role">
+            <div className="block"></div>
+            <p>Full Stack Web Developer</p>
+          </div>
+        </article>
+      </section>
       <div>
         <div id="stars"></div>
         <div id="stars2"></div>
