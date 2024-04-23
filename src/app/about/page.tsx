078af60f26d386dac5374c19fe599ceace5420a6
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-04-24 00:49:13 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,57 +1,89 @@
-
-
-export default function about (){
-    
-    return(
-        <>
-        <section>
-            <header className="title">
-                <h2>About Me</h2>
-            </header>
-            <main>
-                <p>Hey there! I'm Nikolay, a passionate software web developer with a knack for crafting innovative digital solutions.I find my inspiration in the dynamic world of technology and the boundless opportunities it offers.</p>
-            </main>
-        </section>
-        <section>
-            <header>
-                <h3>A Bit About My Journey</h3>
-            </header>
-            <main>
-                <p>Originally from Bulgaria, I've always been fascinated by the intersection of mechanics and software. With a background in Mechanical Engineering, I've honed my skills to marry the precision of engineering with the creativity of web development.</p>
-            </main>
-        </section>
-        <section>
-            <header>
-                <h3>My Passions</h3>
-            </header>
-            <main>
-                <p>When I'm not immersed in lines of code, you'll likely find me indulging in my hobbies. I have a deep love for cars, often spending my weekends tinkering under the hood or hitting the open road. Football matches are a must-watch for me, and I never say no to a thrilling off-road adventure or a karting session.</p>
-            </main>
-        </section>
-        <section>
-            <header>
-                <h3>My Toolbox</h3>
-            </header>
-            <main>
-                <p>In my arsenal, I wield a variety of tools and technologies. From React, Next.js and Angular to Node.js and Express.js, I thrive on the versatility of frameworks and languages like TypeScript and Vanilla JavaScript. I'm adept at handling databases like MongoDB and SQL Server, and I streamline my development process with tools like Postman and GitHub. When it comes to design, I turn to Figma to bring my ideas to life, and for mobile app development, I rely on Ionic.</p>
-            </main>
-        </section>
-        <section>
-            <header>
-                <h3>Experience Beyond Borders</h3>
-            </header>
-            <main>
-                <p>With fluency in both English and German, I've had the privilege of working on projects that span across cultures and continents. My diverse experiences have not only enriched my skill set but also broadened my perspective on problem-solving and collaboration.</p>
-            </main>
-        </section>
-        <section>
-            <header>
-                <h3>Let's Connect</h3>
-            </header>
-            <main>
-                <p>Whether you're looking to build a sleek web application, tackle a complex software challenge, or simply chat about cars and travel, I'm always up for a stimulating conversation. Feel free to reach out and let's embark on this exciting journey together!</p>
-            </main>
-        </section>
-        </>
-    )
-};
+export default function about() {
+  return (
+    <>
+      <section>
+        <header className="title flex justify-center">
+          <h2 className="text-[#ffb000] text-2xl">About Me</h2>
+        </header>
+        <main>
+          <p>
+            Hey there! I'm Nikolay, a passionate software web developer with a
+            knack for crafting innovative digital solutions.I find my
+            inspiration in the dynamic world of technology and the boundless
+            opportunities it offers.
+          </p>
+        </main>
+      </section>
+      <section>
+        <header>
+          <h3>A Bit About My Journey</h3>
+        </header>
+        <main>
+          <p>
+            Originally from Bulgaria, I've always been fascinated by the
+            intersection of mechanics and software. With a background in
+            Mechanical Engineering, I've honed my skills to marry the precision
+            of engineering with the creativity of web development.
+          </p>
+        </main>
+      </section>
+      <section>
+        <header>
+          <h3>My Passions</h3>
+        </header>
+        <main>
+          <p>
+            When I'm not immersed in lines of code, you'll likely find me
+            indulging in my hobbies. I have a deep love for cars, often spending
+            my weekends tinkering under the hood or hitting the open road.
+            Football matches are a must-watch for me, and I never say no to a
+            thrilling off-road adventure or a karting session.
+          </p>
+        </main>
+      </section>
+      <section>
+        <header>
+          <h3>My Toolbox</h3>
+        </header>
+        <main>
+          <p>
+            In my arsenal, I wield a variety of tools and technologies. From
+            React, Next.js and Angular to Node.js and Express.js, I thrive on
+            the versatility of frameworks and languages like TypeScript and
+            Vanilla JavaScript. I'm adept at handling databases like MongoDB and
+            SQL Server, and I streamline my development process with tools like
+            Postman and GitHub. When it comes to design, I turn to Figma to
+            bring my ideas to life, and for mobile app development, I rely on
+            Ionic.
+          </p>
+        </main>
+      </section>
+      <section>
+        <header>
+          <h3>Experience Beyond Borders</h3>
+        </header>
+        <main>
+          <p>
+            With fluency in both English and German, I've had the privilege of
+            working on projects that span across cultures and continents. My
+            diverse experiences have not only enriched my skill set but also
+            broadened my perspective on problem-solving and collaboration.
+          </p>
+        </main>
+      </section>
+      <section>
+        <header>
+          <h3>Let's Connect</h3>
+        </header>
+        <main>
+          <p>
+            Whether you're looking to build a sleek web application, tackle a
+            complex software challenge, or simply chat about cars and travel,
+            I'm always up for a stimulating conversation. Feel free to reach out
+            and let's embark on this exciting journey together!
+          </p>
+        </main>
+      </section>
+    </>
+  );
+}
