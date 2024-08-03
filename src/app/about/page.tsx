1f0a86d968957278977607a62b53789d6f9444a2
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-08-03 13:19:21 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,8 +7,8 @@ export default function about() {
       <section className="">
         <div className="about-wrapper flex justify-center gap-36">
           <div className="image-wrapper h-80 w-80  relative mt-20">
-            <div className="image-border h-80 w-80 absolute top-8 left-8 border-8 rounded-xl -z-50 border-[#ffb000]"></div>
-            <div className="h-80 w-80 overflow-hidden rounded-xl">
+            <div className="image-border w-full	h-full absolute top-8 left-8 border-8 rounded-xl -z-50 border-[#ffb000]"></div>
+            <div className="w-full h-full overflow-hidden rounded-xl">
               <Image
                 className="opacity-100"
                 width={320}
@@ -24,7 +24,7 @@ export default function about() {
               <span className="w-40 h-1 bg-[#ffb000]"></span>
             </header>
             <main>
-              <p className="text-white mt-5">
+              <p className="text-white text-center mt-5">
                 Hey there! I'm Nikolay, a passionate software web developer with
                 a knack for crafting innovative digital solutions.I find my
                 inspiration in the dynamic world of technology and the boundless
@@ -35,7 +35,7 @@ export default function about() {
                   <h3 className="text-[#ffb000] mt-5">
                     A Bit About My Journey
                   </h3>
-                  <p className="text-white mt-5">
+                  <p className="text-white text-center mt-5">
                     Originally from Bulgaria, I've always been fascinated by the
                     intersection of mechanics and software. With a background in
                     Mechanical Engineering, I've honed my skills to marry the
@@ -45,7 +45,7 @@ export default function about() {
                 </div>
                 <div className="flex flex-col items-center flex-1">
                   <h3 className="text-[#ffb000] mt-5">My Passions</h3>
-                  <p className="text-white mt-5">
+                  <p className="text-white text-center mt-5">
                     When I'm not immersed in lines of code, you'll likely find
                     me indulging in my hobbies. I have a deep love for cars,
                     often spending my weekends tinkering under the hood or
@@ -58,7 +58,7 @@ export default function about() {
               <div className="text-wrapper flex gap-8">
                 <div className="flex flex-col items-center flex-1">
                   <h3 className="text-[#ffb000] mt-5">My Toolbox</h3>
-                  <p className="text-white mt-5">
+                  <p className="text-white text-center mt-5">
                     In my arsenal, I wield a variety of tools and technologies.
                     From React, Next.js and Angular to Node.js and Express.js, I
                     thrive on the versatility of frameworks and languages like
@@ -73,7 +73,7 @@ export default function about() {
                   <h3 className="text-[#ffb000] mt-5">
                     Experience Beyond Borders
                   </h3>
-                  <p className="text-white mt-5">
+                  <p className="text-white text-center mt-5">
                     With fluency in both English and German, I've had the
                     privilege of working on projects that span across cultures
                     and continents. My diverse experiences have not only
@@ -84,7 +84,7 @@ export default function about() {
               </div>
 
               <h3 className="text-[#ffb000] mt-5 text-center">Let's Connect</h3>
-              <p className="text-white mt-5">
+              <p className="text-white text-center mt-5">
                 Whether you're looking to build a sleek web application, tackle
                 a complex software challenge, or simply chat about cars and
                 travel, I'm always up for a stimulating conversation. Feel free
