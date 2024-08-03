a5f9466e4a95cb4ee9cd2e88fe7bf258d0a72bdb
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-08-03 13:19:28 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,11 +20,6 @@ export default function Home() {
           </div>
         </article>
       </section>
-      <div>
-        <div id="stars"></div>
-        <div id="stars2"></div>
-        <div id="stars3"></div>
-      </div>
     </>
   );
 }
