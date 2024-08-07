32b26d3565459d016ec19fcca588591dac91fed2
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-08-07 10:30:04 +0300

    Update page.tsx

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,7 +2,8 @@ export default function services (){
     return(
         <>
             <section className="services-page">
-                
+                <h1 className="text-[#ffb000] text-4xl mb-1 text-center">Services</h1>
+                <span className="flex m-auto w-32 h-1 bg-[#ffb000]"></span>
             </section>
         </>
     )
